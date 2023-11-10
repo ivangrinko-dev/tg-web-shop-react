399faba8e35ac9cb678693ddfaf74ba5c30ed4ad
ivangrinko-dev <grinko.work.1982@mail.ru>
2023-11-10 21:25:18 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   }, [])
   
   return (
-    <div className="App">
+    <div>
             <Header />
             <Routes>
                 <Route index element={<ProductList />}/>
